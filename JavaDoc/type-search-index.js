ec6496e23d88df25be3245da63f8022988d1130e
RyanH3 <ryanhwilliams2@gmail.com>
2024-04-10 22:17:14 -0400

    Added JavaDoc

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"AddFrame"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Comic"},{"p":"<Unnamed>","l":"ComicFrame"},{"p":"<Unnamed>","l":"DatabaseManager"},{"p":"<Unnamed>","l":"EditFrame"},{"p":"<Unnamed>","l":"MainFrame"},{"p":"<Unnamed>","l":"RemoveFrame"}];updateSearchResults();
